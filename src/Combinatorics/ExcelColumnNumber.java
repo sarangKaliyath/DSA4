3e483a9e896409c79f0d78def525db77b801552d
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-11-20 05:59:45 +0530

    Combinatrics

--- src/Combinatorics/ExcelColumnNumber.java
+++ src/Combinatorics/ExcelColumnNumber.java
@@ -51,9 +51,38 @@ public class ExcelColumnNumber {
         String str = "AB";
         int res = find(str);
         System.out.println(res);
+        // Time O(length of A);
+        // Space O(1);
+
+        int ans = withoutMathPow(str);
+        System.out.println(ans);
+        // Time O(length of A) - O(time required for Math.pow);
+        // Space O(1);
     }
 
     public static  int find(String str){
-        return 0;
+        int pos = 0;
+        int i = str.length() - 1;
+        int val = 0;
+        while(i >= 0){
+            char el = str.charAt(i);
+            val += (el - 'A' + 1) * Math.pow(26, pos++);
+            i--;
+        }
+        return val;
+    }
+
+    public static  int withoutMathPow(String str){
+        int pos = 0;
+        int i = str.length() - 1;
+        int val = 0;
+        int baseVal = 1;
+        while(i >= 0){
+            char el = str.charAt(i);
+            val += (el - 'A' + 1) * baseVal;
+            baseVal *= 26;
+            i--;
+        }
+        return val;
     }
 }
