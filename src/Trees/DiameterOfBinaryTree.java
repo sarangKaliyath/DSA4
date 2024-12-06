c222c02e4f9ccf5b307de99cbc6a58659f58e3f8
Sakha global <sarang1221@gmail.com>
2024-12-06 20:12:21 +0530

    Trees 5

--- src/Trees/DiameterOfBinaryTree.java
+++ src/Trees/DiameterOfBinaryTree.java
@@ -40,8 +40,29 @@ Explanation 2:
  Longest Path in the tree is 4 -> 2 -> 1 -> 3 -> 6 and the number of edges in this path is 4 so diameter is 4.
  */
 
-public class DiameterOfBinaryTree {
-    public static void main (String [] args){
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class DiameterOfBinaryTree {
+    private static int ansMax = 0;
+
+    public static void main(String[] args) {
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, null, 6));
+        BuildIntegerTree treeBuilder = new BuildIntegerTree();
+        TreeNode root = treeBuilder.BuildIntegerTree(A);
+        int ans = findDiameter(root);
+        System.out.println(ans + " ansMax : " + ansMax);
+        // Time O(N);
+        // Space O(H);
+    }
+
+    public static int findDiameter(TreeNode root) {
+        if (root == null) return 0;
+        int leftHeight = findDiameter(root.left);
+        int rightHeight = findDiameter(root.right);
+        ansMax = Math.max(ansMax, leftHeight + rightHeight);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 }
