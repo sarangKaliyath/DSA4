d265c235814e484739aeec72fe77da08c99f2f45
Sarang <sarang1221@gmail.com>
2025-01-06 18:41:09 +0530

    DP TwoDimensional

---
+++ src/DynamicProgramming/NDigitNumbers.java
@@ -0,0 +1,78 @@
+package DynamicProgramming;
+
+/*
+Problem Description
+Find out the number of A digit positive numbers,
+whose digits on being added equals to a given number B.
+Note that a valid number starts from digits 1-9 except the number 0 itself.
+i.e. leading zeroes are not allowed.
+Since the answer can be large, output answer modulo 1000000007
+
+Problem Constraints
+1 <= A <= 1000
+1 <= B <= 10000
+
+Input Format:
+First argument is the integer A
+Second argument is the integer B
+
+Output Format:
+Return a single integer, the answer to the problem
+
+Example Input:
+Input 1:
+ A = 2
+ B = 4
+
+Input 2:
+ A = 1
+ B = 3
+
+Example Output:
+Output 1: 4
+Output 2: 1
+
+Example Explanation:
+Explanation 1:
+ Valid numbers are {22, 31, 13, 40}
+ Hence output 4.
+
+Explanation 2:
+ Only valid number is 3
+
+ */
+public class NDigitNumbers {
+
+    public static void main(String[] args) {
+        int A = 2;
+        int B = 4;
+
+        int res = iterative(A, B);
+        System.out.println(res);
+        // Time O(A*B);
+        // Space O(A*B);
+    }
+
+    public static int iterative(int A, int B) {
+        int MOD = 1000000007;
+        int[][] dp = new int[A + 1][B + 1];
+
+        for (int col = 1; col <= 9; col++) {
+            if (col <= B) {
+                dp[1][col] = 1;
+            }
+        }
+
+        for (int i = 2; i <= A; i++) {
+            for (int j = 0; j <= B; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    if (j >= k) {
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+                    }
+                }
+            }
+        }
+
+        return dp[A][B];
+    }
+}
